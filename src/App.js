a0f929ddca4173e6e63c575085f11810ae55361a
DanielBarrera <daniel_barrera5@outlook.com>
2020-04-29 22:15:33 -0500

    Se corrigio estilizado en Prueba.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from 'react';   //  /* +tyT */
 import Prueba from './drag_and_drop/Prueba';
 import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 100px;
+`;
+
+const Container = styled.div`
+`;
+
 function App() {
-  return (
-    <div>
-      <h1>Arrastra y suelta</h1>
-      <Prueba/>
-    </div>
-  );
+
+  render(){
+    return (
+      <AppWrapper>
+        <Container>
+          <Prueba />
+        </Container>
+      </AppWrapper>
+    );
+  }
 }
 
 export default App;
--- src/drag_and_drop/Prueba.js
+++ src/drag_and_drop/Prueba.js
@@ -1,44 +1,42 @@
-import React, {Fragment} from 'react';  //  /* +tyT */
+import React from 'react';  //  /* +tyT */
 import styled from 'styled-components';
 import Draggable from './Draggable'; 
 import Droppable from './Droppable';
-import './estilizado/estilos.css';
+//import './estilizado/estilos.css';
 
-//const Wrapper = {
-    //width: '100%',
-    //padding: '32px',
-    //display: 'flex',
-    //justifyContent: 'center'
-//}
+const Wrapper = styled.div`
+    width: 100%;
+    padding: 32px;
+    display: flex;
+    justify-content: center;
+`;
 
-const Item = {
-    padding: '8px',
-    color: '#555',
-    backgroundColor: 'white',
-    borderRadius: '3px'
-}
+const Item = styled.div`
+    padding: 8px;
+    color: #555;
+    background-color: white;
+    border-radius: 3px;
+`;
 
 const droppableStyle = {
     backgroundColor: '#555',
     width: '250px',
     height: '400px',
     margin: '32px'
-}
+};
 
 export default class Prueba extends React.Component {
-
     render(){
         return(
-        <Fragment>
+        <Wrapper>
             <Droppable id="dr1" style={droppableStyle}>
-                <Draggable id="item1" style={{ margin: '8px' }}><Item style={Item}>Item 1</Item></Draggable>
-                <Draggable id="item2" style={{ margin: '8px' }}><Item style={Item}>Item 2</Item></Draggable>
+                <Draggable id="item1" style={{ margin: '8px' }}><Item>Item 1</Item></Draggable>
+                <Draggable id="item2" style={{ margin: '8px' }}><Item>Item 2</Item></Draggable>
             </Droppable>
-
-             <Droppable id="dr2" style={droppableStyle}>
-
+            <Droppable id="dr2" style={droppableStyle}>
+                
             </Droppable>
-        </Fragment>
+        </Wrapper>
         );
     }
     
